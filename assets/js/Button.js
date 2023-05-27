5b36b0bbe345aeb28f11bfb911431336141bd01d
saeed12khan <111329566+saeed12khan@users.noreply.github.com>
2023-05-27 20:14:19 +0500

    commiit

--- assets/js/Button.js
+++ assets/js/Button.js
@@ -1,14 +1,14 @@
-function Submitbtn(){
-    alert('hi');
-    // Email.send({
-    //     Host : "smtp.gmail.com",
-    //     Username : "saeedsaeedkhan089@gmail.com",
-    //     Password : "03121431027Saeed-",
-    //     To : 'saeedsaeedkhan089@gmail.com',
-    //     From : "saeeddon955@gmail.com",
-    //     Subject : "This is the subject",
-    //     Body : "And this is the body"
-    // }).then(
-    //   message => alert(message))
+// function Submitbtn(){
+//     alert('hi');
+//     // Email.send({
+//     //     Host : "smtp.gmail.com",
+//     //     Username : "saeedsaeedkhan089@gmail.com",
+//     //     Password : "03121431027Saeed-",
+//     //     To : 'saeedsaeedkhan089@gmail.com',
+//     //     From : "saeeddon955@gmail.com",
+//     //     Subject : "This is the subject",
+//     //     Body : "And this is the body"
+//     // }).then(
+//     //   message => alert(message))
 
-}
+// }
